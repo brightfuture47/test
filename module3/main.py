6e12f5f6680baf53348e5712b6cb097cf998f516
brightfuture47 <96774402+brightfuture47@users.noreply.github.com>
2024-12-13 12:34:58 +0300

    Create main.py

---
+++ module3/main.py
@@ -0,0 +1,14 @@
+def greet(name):
+    return f'Привет, {name}!'
+
+def add(a, b):
+    return a + b
+
+
+
+if __name__ == '__main__':
+    print(greet('Анна'))
+    print(add(5,3))
+    print(__name__)
+
+
